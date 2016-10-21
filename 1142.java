0fc432d02a590ff53990a4c144b487526d2a39f0
MarkosVS <markos.vs@live.com>
2016-10-21 08:58:31 -0300

    Add files via upload

---
+++ 1142.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class uri1142{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt(), j = 1;
+        for(int i = 0; i < n; i++){
+            System.out.println(j + " " + (j+1) + " " + (j+2) + " PUM");
+            j+=4;
+        }
+
+    }
+}
