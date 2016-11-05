4fc76f48eb5d8b998ac1ceaf01256db64d9ccb8b
MarkosVS <markos.vs@live.com>
2016-11-05 18:47:33 -0300

    Add files via upload

---
+++ 1589 - Bob Conduite.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class uri1541{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt(), r1, r2; 
+        while(n-- > 0){
+            r1 = scan.nextInt();
+            r2 = scan.nextInt();
+            System.out.println(r1 + r2);
+        }
+    }
+}
