2f1ff95ccea55ad8923f33ce064c118625daad27
MarkosVS <markos.vs@live.com>
2016-11-06 12:07:30 -0300

    Add files via upload

---
+++ 1759 - Ho Ho Ho.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class uri1759{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt(), i = 1; 
+        while(i<n){
+            System.out.print("Ho ");
+            i++;
+        }
+        System.out.println("Ho!");
+    }
+}
