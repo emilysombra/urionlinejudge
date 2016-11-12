1abc95b5f64d191e683d0bf0cdeda4250be64cba
MarkosVS <markos.vs@live.com>
2016-11-12 09:05:40 -0300

    Add files via upload

---
+++ 1924 - Vitória e a Indecisão.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class uri1924{
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args){
+        int n = scan.nextInt();
+        String s;
+        while(n-- > 0)
+            s = scan.nextLine();   
+        System.out.println("Ciencia da Computacao");
+    }
+}
