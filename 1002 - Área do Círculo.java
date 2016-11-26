479c1ebde8c33c659f25073807188fb6d105a769
Marcos Vinicius Sombra <markos.vs@live.com>
2016-11-26 08:42:22 -0300

    Update 1002 - Área do Círculo.java

--- 1002 - Área do Círculo.java
+++ 1002 - Área do Círculo.java
@@ -1,13 +1,10 @@
-import java.util.Scanner;
-import java.util.*;  
-class uri1002{
+import java.util.Scanner; 
+class Main{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		double raio, area;
 		raio = scan.nextDouble();
 		area = 3.14159 * (Math.pow(raio,2));
-		System.out.print("A=");
-		System.out.printf("%.4f", area);
-		System.out.print("\n");
+		System.out.printf("A=%.4f\n", area);
 	}
 }
