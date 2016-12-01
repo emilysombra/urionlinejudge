232663264a1466aa308e50f1192573411e4e370c
Marcos Vinicius Sombra <markos.vs@live.com>
2016-12-01 09:09:31 -0300

    Update 1005 - Média 1.java

--- 1005 - Média 1.java
+++ 1005 - Média 1.java
@@ -2,12 +2,10 @@ import java.util.Scanner;
 class uri1005{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
-		double A, B, MEDIA;
-		A = scan.nextDouble();
-		B = scan.nextDouble();
-		MEDIA = ((A * 3.5) + (B * 7.5))/11;
-		System.out.print("MEDIA = ");
-		System.out.printf("%.5f", MEDIA);
-		System.out.print("\n");
+		double a, b, med;
+		a = scan.nextDouble();
+		b = scan.nextDouble();
+		med = ((a * 3.5) + (b * 7.5))/11;
+		System.out.printf("MEDIA = %.5f\n", med);
 	}
 }
