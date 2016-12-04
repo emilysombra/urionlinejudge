7884c16379201dcf7015241ccab5fffb2b9461f6
Marcos Vinicius Sombra <markos.vs@live.com>
2016-12-04 08:14:49 -0300

    Update 1011 - Esfera.java

--- 1011 - Esfera.java
+++ 1011 - Esfera.java
@@ -1,18 +1,10 @@
 import java.util.Scanner;
-class uri1010{
+class Main{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
-		int qtd1, qtd2, cod1, cod2;
-		double valor1, valor2, valorFinal;
-		cod1 = scan.nextInt();
-		qtd1 = scan.nextInt();
-		valor1 = scan.nextDouble();
-		cod2 = scan.nextInt();
-		qtd2 = scan.nextInt();
-		valor2 = scan.nextDouble();
-		valorFinal = (qtd1 * valor1) + (qtd2 * valor2);
-		System.out.print("VALOR A PAGAR: R$ ");
-		System.out.printf("%.2f", valorFinal);
-		System.out.print("\n");
+		double raio, volume;
+		raio = scan.nextDouble();
+		volume = (4.0/3) * 3.14159 * raio * raio * raio;
+		System.out.printf("VOLUME = %.3f\n", volume);
 	}
 }
