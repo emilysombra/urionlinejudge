e1fbbabf56507e2247db4ff665d5f6497a493dcb
Marcos Vinicius Sombra <markos.vs@live.com>
2016-12-24 13:46:00 -0300

    Update 1013 - O Maior.java

--- 1013 - O Maior.java
+++ 1013 - O Maior.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class uri1013{
+class Main{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		int a, b, c;
@@ -9,14 +9,11 @@ class uri1013{
 		c = scan.nextInt();
 		maiorAB = (a + b + (Math.abs(a - b)))/2;
 		maiorABC = (maiorAB + c + (Math.abs(maiorAB - c)))/2;
-		if(maiorABC == a){
-			System.out.print(a + " eh o maior\n");
-		}
-		if(maiorABC == b){
-			System.out.print(b + " eh o maior\n");
-		}
-		if(maiorABC == c){
-			System.out.print(c + " eh o maior\n");
-		}
+		if(maiorABC == a)
+			System.out.println(a + " eh o maior");
+		else if(maiorABC == b)
+			System.out.println(b + " eh o maior");
+		else if(maiorABC == c)
+			System.out.println(c + " eh o maior");
 	}
 }
