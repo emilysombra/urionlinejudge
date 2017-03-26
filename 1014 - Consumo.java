ba93c3e6e7cd5dd747ba49e1d5e9211cb5cdb065
Marcos Vinicius Sombra <markos.vs@live.com>
2017-03-26 11:46:10 -0300

    Update 1014 - Consumo.java

--- 1014 - Consumo.java
+++ 1014 - Consumo.java
@@ -1,13 +1,9 @@
 import java.util.Scanner;
-class uri1014{
+public class Main{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
-		int dist;
-		double totalKm, consumo;
-		dist = scan.nextInt();
-		totalKm = scan.nextDouble();
-		consumo = dist / totalKm;
-		System.out.printf("%.3f", consumo);
-		System.out.print(" km/l\n");
+		int dist = scan.nextInt();
+		double totalKm = scan.nextDouble();
+		System.out.printf("%.3f km/l\n", dist / totalKm);
 	}
 }
