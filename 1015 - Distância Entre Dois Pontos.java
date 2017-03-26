41a86a076283d0372a22909f616fbd9dfdb6b9d5
Marcos Vinicius Sombra <markos.vs@live.com>
2017-03-26 11:49:16 -0300

    Update 1015 - Distância Entre Dois Pontos.java

--- 1015 - Distância Entre Dois Pontos.java
+++ 1015 - Distância Entre Dois Pontos.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class uri1015{
+public class Main{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		double x1, x2, y1, y2, dist, x, y;
@@ -7,10 +7,7 @@ class uri1015{
 		y1 = scan.nextDouble();
 		x2 = scan.nextDouble();
 		y2 = scan.nextDouble();
-		x = x2 - x1;
-		y = y2 - y1;
-		dist = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
-		System.out.printf("%.4f", dist);
-		System.out.print("\n");
+		dist = Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
+		System.out.printf("%.4f\n", dist);
 	}
 }
