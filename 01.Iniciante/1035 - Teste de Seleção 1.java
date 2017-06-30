82bbbe9f895cc2c0251b93bbc11f1f1026c2b745
Marcos Sombra <markos.vs@live.com>
2017-06-30 14:03:39 -0300

    melhorando solução

--- 01.Iniciante/1035 - Teste de Seleção 1.java
+++ 01.Iniciante/1035 - Teste de Seleção 1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class uri1035{
+public class Main{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		int a, b, c, d;
@@ -7,11 +7,9 @@ class uri1035{
 		b = scan.nextInt();
 		c = scan.nextInt();
 		d = scan.nextInt();
-		if(b>c && d>a && (c+d)>(a+b) && c>0 && d>0 && a%2==0){
-			System.out.print("Valores aceitos\n");
-		}else{
-			System.out.print("Valores nao aceitos\n");
-		}
-		
+		if(b > c && d > a && (c + d) > (a + b) && c > 0 && d > 0 && a%2 == 0)
+			System.out.println("Valores aceitos");
+		else
+			System.out.println("Valores nao aceitos");
 	}
 }
